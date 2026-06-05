90ea916c8d387e70b59f435615cf2f4b4803eb0e
Guillermo Álvarez <hardwarize@gmail.com>
2026-06-05 10:04:07 +0000

    final optim

--- depth_estimation/utils/data.py
+++ depth_estimation/utils/data.py
@@ -266,6 +266,92 @@ class InputTargetDataset:
 
 
 
+
+def check_dataset(path_tuples):
+        """Checks dataset for missing files."""
+        for tuple in path_tuples:
+            for f in tuple:
+                if not exists(f):
+                    print(f"Missing file: {f}.")
+                    return False
+
+        print(f"Checked {len(path_tuples)} tuples for existence, all ok.")
+
+        return True
+
+def read_features(path, max_priors, device="cpu"):
+        """Read sparse priors from file and store in torch tensor."""
+
+        # load samples (might be less than n_samples)
+        depth_samples_data = pd.read_csv(path).to_numpy()
+
+        # give warning when no features
+        if len(depth_samples_data) == 0:
+            print(f"WARNING: Features list {path} is empty, returning None!")
+            return None
+        else:
+            rand_idcs = np.random.permutation(len(depth_samples_data))[
+                : max_priors
+            ]
+            depth_samples = depth_samples_data[rand_idcs]  # select subset
+
+        # tensor from numpy
+        depth_samples = torch.from_numpy(depth_samples).to(device)
+
+        return depth_samples
+
+
+class InputDataset:
+    """Similar to InputTargetDataset above, but for inference only. If priors are not available, set `max_priors` to zero."""
+
+    def __init__(self, rgb_priors_tuples, image_transform, max_priors=200) -> None:
+        
+        self.path_tuples = rgb_priors_tuples
+        self.image_transform = image_transform
+        self.max_priors = max_priors
+
+        if self.max_priors > 0:
+            print(f"Using priors (max {self.max_priors} per image).")
+            check_dataset(self.path_tuples)
+        else: 
+            image_fns = [[t[0]] for t in self.path_tuples]
+            print("Not using priors, using nullprior as placeholder.")
+            check_dataset(image_fns)
+
+    def __len__(self):
+        return len(self.path_tuples)
+    
+    def __getitem__(self, idx):
+
+        # get img filename
+        input_fn = self.path_tuples[idx][0]
+
+        # read img
+        input_img = Image.open(input_fn).resize((640, 480))
+
+        # apply image transforms to get tensor
+        input_img = self.image_transform(input_img)
+
+        if self.max_priors > 0:
+
+            # get keypoints filename
+            depth_samples_fn = self.path_tuples[idx][1]
+
+            # read sparse depth priors
+            depth_samples = read_features(depth_samples_fn, self.max_priors, device=input_img.device)
+            
+            # get dense parametrization from sparse priors
+            parametrization = get_depth_prior_from_features(
+                features=depth_samples.unsqueeze(0),  # add batch dimension
+                height=240,
+                width=320,
+            ).squeeze(0)
+
+            return [input_img, parametrization]
+        else:
+            return [input_img]
+
+
 class MutualRandomHorizontalFlip:
     """Randomly flips an input RGB imape and corresponding depth target horizontally with probability p.\\
     (Either both are transformed or neither of them)"""
